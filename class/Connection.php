41feca17ac46bcebe580e9f22a2062a40244fd27
Waqas Ahmad <waqaskanju@gmail.com>
2019-07-09 12:03:07 +0500

    Update Connection.php

--- class/Connection.php
+++ class/Connection.php
@@ -1,1 +1,22 @@
-<?phpclass Connection{                         private  static $hostname='localhost';                     private  static  $dbname='b16_18989341_flatdb';                     private  static $username='root';                     private  static $password='';    public  static $con;    public  function  __construct(){        self::$con=mysqli_connect(self::$hostname,self::$username,self::$password,self::$dbname);        return self::$con;    }}$obj= new Connection();$con=Connection::$con;
+<?php
+
+class Connection
+{
+    
+                     private  static $hostname='localhost';
+                     private  static  $dbname='flatdb';
+                     private  static $username='root';
+                     private  static $password='password';
+
+
+
+
+    public  static $con;
+    public  function  __construct(){
+        self::$con=mysqli_connect(self::$hostname,self::$username,self::$password,self::$dbname);
+        return self::$con;
+    }
+}
+
+$obj= new Connection();
+$con=Connection::$con;
